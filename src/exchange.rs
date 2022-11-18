cf9a58155b98ae4eb45975624eb6a866e916af13
jasongoodwin <jay.michael.goodwin@gmail.com>
2022-11-17 20:17:01 -0800

    fix(exchange connections): remove some unwraps that would panic

--- src/exchange.rs
+++ src/exchange.rs
@@ -16,7 +16,7 @@ mod bitstamp;
 
 #[derive(Debug, PartialEq)]
 pub struct OrderBookUpdate {
-    // Timestamp of message receipt to track to delivery (metrics)
+    // Timestamp of OrderBookUpdate creation for metrics. This is a little late but captures our code.
     pub(crate) ts: Instant,
     pub(crate) exchange: String,
     // Note: we use the Level struct which will duplicate the exchange in each level.
@@ -38,7 +38,7 @@ trait Exchange {
 // 2. refactor to test message receipt/reply.
 // 3. If we don't get a message in x period of time, should probably close connection and re-establish.
 
-/// produces a thread to establish and manage connection/subscription to an exchange.
+/// produces a thread to establish and manages connection/subscription to an exchange.
 pub async fn create_exchange_ws_connection(
     exchange_config: ExchangeConfig,
     subscribers_tx: mpsc::Sender<OrderBookUpdate>,
@@ -56,7 +56,6 @@ pub async fn create_exchange_ws_connection(
         // anymore if failures are encountered.
         // Eg if the exchange endpoint goes down, we want to signal that there are no bids/asks for
         // the exchange available until we re-establish stability.
-
         loop {
             // outer loop will establish connection
             info!(
@@ -73,19 +72,36 @@ pub async fn create_exchange_ws_connection(
                 id => Err(format!("unsupported exchange configured: {}", id)).unwrap(), // will crash the app.
             };
 
-            let (mut ws_stream, _) = connect_async(exchange_config.endpoint.clone())
-                .await
-                .expect("Failed to connect");
+            let (mut ws_stream, _) = match connect_async(exchange_config.endpoint.clone()).await {
+                Ok(x) => x,
+                Err(_) => {
+                    error!(
+                        "issue connecting to {}... Will retry.",
+                        exchange_config.id.as_str()
+                    );
+                    sleep(Duration::from_millis(100)).await; // wait 100ms to avoid hammering a failing endpoint.
+                    continue;
+                }
+            };
+
             info!(
                 "WebSocket handshake has been successfully completed for {}",
                 exchange_config.id.as_str()
             );
 
             // subscribe
-            ws_stream
+            if !ws_stream
                 .send(Message::text(exchange.subscribe_msg()))
                 .await
-                .unwrap(); // TODO unsafe
+                .is_ok()
+            {
+                error!(
+                    "issue sending subscribe for {}... Will retry.",
+                    exchange_config.id.as_str()
+                );
+                sleep(Duration::from_millis(100)).await; // wait 100ms to avoid hammering a failing endpoint.
+                continue;
+            }
 
             // Get the reply message. If anything not as expected, we just continue the loop w/ a delay.
             match ws_stream.next().await {
@@ -95,7 +111,7 @@ pub async fn create_exchange_ws_connection(
                     continue;
                 }
                 Some(Ok(msg)) => {
-                    // Todo validate the subscription reply as expected. Needs to be done per exchange.
+                    // FIXME need validate the subscription reply is as expected. implement in Exchange trait.
                     info!("got subscription reply {:?}", msg.to_string())
                 }
                 Some(Err(e)) => {
@@ -118,20 +134,28 @@ pub async fn create_exchange_ws_connection(
                             "ping frame received for {}. Sending pong",
                             exchange_config.id.as_str()
                         );
-                        ws_stream.send(Pong(vec![])).await.unwrap(); // TODO unsafe unwrap.
+                        if ws_stream.send(Pong(vec![])).await.is_err() {
+                            error!(
+                                "error sending ping reply to {}... Will retry disconnect/retry.",
+                                exchange_config.id.as_str()
+                            );
+                            break;
+                        }
                     }
                     Some(Ok(msg)) => {
                         match exchange.parse_order_book_data(msg.into_data()) {
                             Ok(order_book_update) => {
                                 // can possibly spawn this instead of awaiting, but need to ensure order.
-                                subscribers_tx.send(order_book_update).await.unwrap();
-                                // TODO unsafe unwrap
+                                subscribers_tx
+                                    .send(order_book_update)
+                                    .await
+                                    .expect("unexpected error sending to channel. Panic!");
                             }
                             Err(e) => {
                                 subscribers_tx
                                     .send(exchange.empty_order_book_data())
                                     .await
-                                    .unwrap(); // TODO unsafe unwrap.
+                                    .expect("unexpected error sending to channel. Panic!");
                                 error!("Restarting connection as we couldn't parse an orderbook update for {}!: {:?}", exchange_config.id.as_str(), e);
                                 break;
                             }
@@ -151,7 +175,7 @@ pub async fn create_exchange_ws_connection(
             subscribers_tx
                 .send(exchange.empty_order_book_data())
                 .await
-                .unwrap(); // TODO unsafe unwrap?
+                .expect("unexpected error sending to channel");
             sleep(Duration::from_millis(100)).await; // wait 100ms to avoid hammering a failing endpoint.
         }
     });
